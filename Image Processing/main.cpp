a1b4a81b691babc704b51011b75f8eb94202c549
EmilValberg <36535221+EmilValberg@users.noreply.github.com>
2018-09-18 11:12:47 +0200

    Test code and image.

--- Image Processing/main.cpp
+++ Image Processing/main.cpp
@@ -1,1 +1,9 @@
 #include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argv, char** argc)
+{
+	Mat test = imread("testpat.jpg", CV_LOAD_IMAGE_UNCHANGED);
+	imshow("Test", test);
+	waitKey();
+}
