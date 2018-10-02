59d9c3a70c7f88db5abb67b660eb88dc6623565c
Simon Laursen <simonvestergaardlaursen@gmail.com>
2018-10-02 14:26:57 +0200

    Solution with project inside. A small hello world presentation is there to ease you into the language.

--- ComputerVision/Program.cs
+++ ComputerVision/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ComputerVision
+﻿using System;
+
+namespace ComputerVision
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press button to exit");
+            Console.ReadKey();
         }
     }
 }
