b144ddef87c098186390f64bbc79a6ea984843be
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2018-11-07 13:59:32 +0100

    Fix

--- ComputerVision/ShapeDetector/Program.cs
+++ ComputerVision/ShapeDetector/Program.cs
@@ -7,20 +7,17 @@ using System.IO;
 namespace ShapeDetector
 {   
     internal class Program
-    {   
+    {
         public static void Main(string[] args)
         {
-            String root = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            String rootPath = Path.GetFullPath(Path.Combine(root, ".."));
-            List<Color> _c = new List<Color>();
-            List<Blob> _b = new List<Blob>();
-            Color red = Color.FromArgb(255, 255, 0, 0);
-            _c.Add(Color.Red);
-            _c.Add(Color.Blue);
-            _c.Add(Color.Green);
+            CommandConsole.Run();
+        }
+            
 
         public void StartProgram()
         {
+            //String root = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //String rootPath = Path.GetFullPath(Path.Combine(root, ".."));
             //List<Color> _c = new List<Color>();
             //List<Blob> _b = new List<Blob>();
             //Color red = Color.FromArgb(255, 255, 0, 0);
@@ -34,11 +31,10 @@ namespace ShapeDetector
             //CCHandler CC = new CCHandler(_c, _img);
 
             //Bitmap _timg = CC.BackgroundThreshold(_img, 255);
-            _b = CC.Detect(_img, 150);
-            Console.WriteLine(_b.Count);
-            Blob.DrawBlobs(_img, _b);
-            _img.Save(rootPath+ "/Export/blob.bmp");
-            Console.ReadKey();
+            //_b = CC.Detect(_img, 150);
+            //Console.WriteLine(_b.Count);
+            //Blob.DrawBlobs(_img, _b);
+            //_img.Save(rootPath+ "/Export/blob.bmp");
             
         }
     }
