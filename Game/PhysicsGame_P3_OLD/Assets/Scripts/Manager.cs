9dde5e60747f629f846862b0213170f4b790f2d3
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2018-11-13 15:02:51 +0100

    Fixed Unity version

---
+++ Game/PhysicsGame_P3_OLD/Assets/Scripts/Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+	public bool gameActive;
+
+	private Player _player;
+	
+	#region Singleton
+	private static Manager _instance;
+
+	public static Manager Instance { get { return _instance; } }
+
+	private void Awake()
+	{
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+		} 
+		else {
+			_instance = this;
+		}
+
+		_player = GameObject.FindWithTag("Player").GetComponent<Player>();
+	}
+	#endregion
+	
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(0)) gameActive = !gameActive;
+
+		if (!gameActive)
+		{
+			_player.isPaused = true;
+		}
+		else
+		{
+			if (WinCheck()) GameComplete();
+			_player.isPaused = false;
+		}
+	}
+
+	private void GameComplete()
+	{
+		//TODO: What to do once the game is completed
+		Debug.Log("LEVEL COMPLETE");
+	}
+
+	private bool WinCheck()
+	{
+		return _player.hitGoal;
+	}
+}
