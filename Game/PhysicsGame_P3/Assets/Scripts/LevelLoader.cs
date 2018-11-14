c4ef46689648041f84fc16f5a04fe67f8ef41979
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2018-11-14 14:36:36 +0100

    LoadImage added. Needs further refinement for our usage.
    Unity project output was changed to windows when it was loaded. Most of the file changes are due to that.
    The important additions are in LevelLoader, line 76 and forward. The new loadImage function loads files from a given filepath, and outputs a Texture2D. Getting the path to the StreamingAssets folder is mostly a matter of calling Application.dataPath. 
    UseTestImgAsTexture.cs was made for testing the function.
    We have a problem here, however. Unity Engine can only load JPG's and PNG's. A fix would probably be to handle the BMP with System.Drawing instead. 
    Discussion needed.

--- Game/PhysicsGame_P3/Assets/Scripts/LevelLoader.cs
+++ Game/PhysicsGame_P3/Assets/Scripts/LevelLoader.cs
@@ -4,79 +4,103 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
-	public Texture2D levelMap;
-	public bool loadFromBytes;
-	public string levelFileName;
+    public Texture2D levelMap;
+    public bool loadFromBytes;
+    public string levelFileName;
 
-	public List<ColorToPrefab> entities = new List<ColorToPrefab>();
+    public List<ColorToPrefab> entities = new List<ColorToPrefab>();
 
-	public void LoadLevel()
-	{
-		EmptyMap(); //Reset before starting the load process
-		
-		if(levelMap == null) Debug.LogError("No map loaded");
+    public void LoadLevel()
+    {
+        EmptyMap(); //Reset before starting the load process
 
-		if (loadFromBytes)
-		{
-			//Loads file as bytes from StreamingAssets folder (Maybe change to be the only possible way?)
-			var filePath = Application.dataPath + "/StreamingAssets/" + levelFileName;
-			byte[] bytes = System.IO.File.ReadAllBytes(filePath);
-			levelMap.LoadImage(bytes);
-		}
+        if (levelMap == null) Debug.LogError("No map loaded");
 
-		var allPixels = levelMap.GetPixels32();
-		var width = levelMap.width;
-		var height = levelMap.height;
+        if (loadFromBytes)
+        {
+            //Loads file as bytes from StreamingAssets folder (Maybe change to be the only possible way?)
+            var filePath = Application.dataPath + "/StreamingAssets/" + levelFileName;
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+            levelMap.LoadImage(bytes);
+        }
 
-		for (var x = 0; x < width; x++)
-		{
-			for (var y = 0; y < height; y++)
-			{
-				SpawnAt(allPixels[x + (y * width)], x, y);
-			}
-		}
-	}
+        var allPixels = levelMap.GetPixels32();
+        var width = levelMap.width;
+        var height = levelMap.height;
 
-	public void EmptyMap()
-	{
-		while (transform.childCount > 0)
-		{
-			var c = transform.GetChild(0);
-			c.SetParent(null);
-			Destroy(c.gameObject);
-		}
-	}
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                SpawnAt(allPixels[x + (y * width)], x, y);
+            }
+        }
+    }
 
-	public void SetMap(Texture2D map)
-	{
-		levelMap = map;
-	}
+    public void EmptyMap()
+    {
+        while (transform.childCount > 0)
+        {
+            var c = transform.GetChild(0);
+            c.SetParent(null);
+            Destroy(c.gameObject);
+        }
+    }
 
-	private void SpawnAt(Color32 color, int x, int y)
-	{
-		if (color == Color.black) return;
-		
-		//Find the right color in our map
-		foreach (var ctp in entities)
-		{
-			if(!color.Equals(ctp.color)) continue;
-			
-			//Spawn prefab at right location according to map 
-			//Important! The last part of input in the below variable determines rotation!
-			var obj = Instantiate(ctp.prefab, new Vector3(x, y, 0), Quaternion.identity);
-			obj.transform.SetParent(transform);
-			
-			return;
-		}
-		
-		//Did not find matching prefab to input color
-		Debug.LogError("No prefab found for " + color);
-	}
+    public void SetMap(Texture2D map)
+    {
+        levelMap = map;
+    }
+
+    private void SpawnAt(Color32 color, int x, int y)
+    {
+        if (color == Color.black) return;
+
+        //Find the right color in our map
+        foreach (var ctp in entities)
+        {
+            if (!color.Equals(ctp.color)) continue;
+
+            //Spawn prefab at right location according to map 
+            //Important! The last part of input in the below variable determines rotation!
+            var obj = Instantiate(ctp.prefab, new Vector3(x, y, 0), Quaternion.identity);
+            obj.transform.SetParent(transform);
+
+            return;
+        }
+
+        //Did not find matching prefab to input color
+        Debug.LogError("No prefab found for " + color);
+    }
+
+    //NOTE: to get the location of the StreamingAssets folder, Application.datapath has to be used.
+    //public static string streamingAssetsPath = Application.dataPath + "/StreamingAssets";
+    //public static string blobImageFileName = "testImg.bmp";
+
+    public static Texture2D loadImage(string filePath)
+    { //NOTE: Sadly, the Unity Engine (not editor!) cannot load .bmp's, only .png's and .jpg's. If given anything else, it will return a standard 8x8 texture with a question mark.
+      //Other than that, this function works, and will load images from a given filepath
+        Debug.Log("Looking for image: " + filePath);
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (System.IO.File.Exists(filePath))
+        {
+            Debug.Log("Found file");
+            fileData = System.IO.File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2, TextureFormat.BGRA32, false);
+            Debug.Log(tex.width);
+            tex.LoadImage(fileData); //texture will be automatically resized.
+            Debug.Log(tex.width);
+        }
+
+        return tex;
+    }
 }
 
 [System.Serializable]
 public struct ColorToPrefab
 {
-	public Color32 color;
-	public GameObject prefab;
+    public Color32 color;
+    public GameObject prefab;
 }
