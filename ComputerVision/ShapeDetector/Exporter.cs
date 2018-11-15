d044c69e8e91f056ec65519ab3bc3c94e49cb308
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2018-11-15 13:15:46 +0100

    Start of blob data exporter. Merging with new blob object functions.

---
+++ ComputerVision/ShapeDetector/Exporter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+
+namespace ShapeDetector
+{
+
+    public class Exporter
+    {
+        public void Run(Blob[] blobs, string target, string filename)
+        {
+            BinaryWriter bw;
+
+            try
+            {
+                bw = new BinaryWriter(new FileStream(target + "\\" + filename, FileMode.Create));
+            }
+            catch (IOException e){
+                Console.WriteLine(e.Message + "\n Cannot write file.");
+                return;
+            }
+
+            try {
+                foreach (Blob b in blobs)
+                {
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot write to file.");
+                return;
+            }
+            bw.Close();
+
+        }
+    }
+}
