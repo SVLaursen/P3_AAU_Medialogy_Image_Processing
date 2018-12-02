db93064d7073ef4de8f2396b2538953c068d60f7
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2018-12-02 14:05:29 +0100

    Added the Main Menu to the game

---
+++ Game/PhysicsGame_P3/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public GameObject levelSelector;
+	public GameObject mainMenu;
+
+	private void Start()
+	{
+		Main();
+	}
+	
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void LevelMenu()
+	{
+		levelSelector.SetActive(true);
+		mainMenu.SetActive(false);
+	}
+
+	public void Main()
+	{
+		levelSelector.SetActive(false);
+		mainMenu.SetActive(true);
+	}
+
+	public void SelectLevel(int index)
+	{
+		SceneManager.LoadScene(index);
+	}
+}
